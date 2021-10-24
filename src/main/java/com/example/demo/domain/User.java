71b17ff715993cf8c88eca8b2ad365ee6b20cbef
mcamore <mmco94@live.com>
2021-10-24 21:00:40 +0200

    User domain model and JPA mapping

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -1,2 +1,25 @@
-package com.example.demo.domain;public class User {
+package com.example.demo.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @SequenceGenerator(
+            name = "users_sequence",
+            sequenceName = "users_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.AUTO,
+            generator = "users_sequence"
+    )
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
 }
