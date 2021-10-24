90287cfc251d83964b0c864a50d9fcfabd10dcc0
mcamore <mmco94@live.com>
2021-10-24 18:50:07 +0200

    Switched snake case to camel case in DB user table definition properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,9 @@ DROP TABLE IF EXISTS USERS;
 
 CREATE TABLE USERS (
   id INT AUTO_INCREMENT  PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL,
+  firstName VARCHAR(250) NOT NULL,
+  lastName VARCHAR(250) NOT NULL,
   email VARCHAR(250) DEFAULT NULL
 );
 
-INSERT INTO USERS (first_name, last_name, email) VALUES ('Peter', 'Parker', 'spiderman@gmail.com');
+INSERT INTO USERS (firstName, lastName, email) VALUES ('Peter', 'Parker', 'spiderman@gmail.com');
